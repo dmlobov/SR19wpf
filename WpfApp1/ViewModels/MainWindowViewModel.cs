b294ae89db855bd861b7208bae2eac1128131ed0
D L <d.m.lobov@yandex.ru>
2023-01-12 17:29:51 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                if (value < 0)
+                    number1 = value*(-1);
+                else
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number2 = Ariph.Add(Number1);
+        }
+        private bool CanAddCommandExecute(object p)
+        {
+            if (Number1 != 0)
+                return true;            
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
